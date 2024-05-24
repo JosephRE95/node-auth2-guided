b5f30961ba118c7ff0aae787cf3801c4cc04272d
Joseph Ellis <josephellis95@gmail.com>
2024-05-24 11:53:27 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,21 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../../config/')
+
 // AUTHENTICATION
-const restricted = (req, res, next) => {
-  next()
+const restricted = (req, res, next) => { 
+  const token = req.headers.authorization 
+  if (token){
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        next({status:401, message: `token bad: ${err.message}`})
+      }
+    })
+  } else {
+    req.decodedJwt = decoded
+    console.log(req.decodedJwt) 
+    next()
+  }
+  next({  status:401, message: "no no no!" })
 }
 
 // AUTHORIZATION
